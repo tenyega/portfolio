3d729d73666bfacea67d2f5c589fd622c9efcafe
yega <mdolma@ymail.com>
2024-06-30 22:15:08 +0200

    changes in images hackathon

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -20,7 +20,7 @@ export default function Project({ prop }: Props) {
     const projectList = [
         {
             link:"https://hackathonfinal-kq82swkkl-tenyegas-projects.vercel.app/",
-            image: "http://via.placeholder.com/400",
+            image: "./hackathon.png",
             name: "Hackathon",
             desc: "Detection dobject en live et de lister et filter.  ",
             techno: [ "All",  "HTML" ,"CSS","JS","IndexDB"]
